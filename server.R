fa999b2ab0a85c1cd79ed9b940931b564531f828
yichenlu47 <luyichen1994@gmail.com>
2021-04-05 16:00:47 -0700

    add more dist

--- server.R
+++ server.R
@@ -1,4 +1,6 @@
 library(shiny)
+library(ggplot2)
+library(MASS)
 
 # Define server logic required to summarize and view the selected dataset
 shinyServer(function(input, output) {
@@ -11,13 +13,36 @@ shinyServer(function(input, output) {
   output$gamma_pdf <- renderUI({
     withMathJax(helpText('$$f(x) = \\frac{1}{\\gamma(\\alpha)\\beta^{\\alpha}} x^{\\alpha-1} \\exp{-\\frac{x}{\\beta}}$$'))
   })
+  output$cauchy_pdf <- renderUI({
+    withMathJax(helpText('$$f(x) = \\frac{1}{\\gamma(\\alpha)\\beta^{\\alpha}} x^{\\alpha-1} \\exp{-\\frac{x}{\\beta}}$$'))
+  })
+  output$bern_pmf <- renderUI({
+    withMathJax(helpText('$$p(x) = p^x(1-p)^{1-x}$$'))
+  })
+  output$binom_pmf <- renderUI({
+    withMathJax(helpText('$$p(x) =C^{n}_{x} p^x(1-p)^{1-x}$$'))
+  })
+  output$geom_pmf <- renderUI({
+    withMathJax(helpText('$$p(x) =(1-p)^{x-1} p$$'))
+  })
+  output$negbinom_pmf <- renderUI({
+    withMathJax(helpText('$$p(x) =(1-p)^{x-1} p$$'))
+  })
+  output$pois_pmf <- renderUI({
+    withMathJax(helpText('$$p(x) = \\frac{1}{x!} exp^{-\\lambda} \\lambda^x$$'))
+  })
   
   # Return the requested dataset
   dataInput <- reactive({
     switch(input$dist,
-           "uniform" = runif(input$n, min = input$unif_min, max = input$unif_max),
-           "gaussian" = rnorm(input$n, mean = input$gaus_mean, sd = sqrt(input$gaus_var)),
-           "gamma" = rgamma(input$n, shape = input$gamma_shape, scale = input$gamma_scale)
+           "uniform" = runif(input$obs, min = input$unif_min, max = input$unif_max),
+           "gaussian" = rnorm(input$obs, mean = input$gaus_mean, sd = sqrt(input$gaus_var)),
+           "gamma" = rgamma(input$obs, shape = input$gamma_shape, scale = input$gamma_scale),
+           "cauchy" = rcauchy(input$obs, location = input$cauc_loc, scale = input$cauc_scale),
+           "bernoulli" = rbinom(input$obs, size = 1, prob = input$bern_prob),
+           "binomial" = rbinom(input$obs, size = input$binom_n, prob = input$binom_prob),
+           "geometric" = rgeom(input$obs, prob = input$binom_prob),
+           "poisson" = rpois(input$obs, lambda = input$pois_lambda)
     )
   })
   
@@ -26,12 +51,23 @@ shinyServer(function(input, output) {
     data <- dataInput()
     summary(data)
   })
-  
+  # 
   output$distPlot <- renderPlot({
     data <- dataInput()
     # generate an rnorm distribution and plot it
     hist(data, breaks = 50)
   })
+  
+  output$ggPlot <- renderPlot({
+    data <- as.data.frame(dataInput())
+    colnames(data) = "x"
+    ggplot(data, aes(x=x)) + geom_density(alpha=0.4, fill="lightskyblue")+
+      geom_vline(aes(xintercept=mean(x)), color="deepskyblue",
+                 linetype="dashed", size = 1)+
+      labs(title="Density curve", y = "Density")+
+      theme_classic()
+  })
+  
   
   # Show the first "n" observations
   # output$view <- renderTable({
--- ui.R
+++ ui.R
@@ -10,38 +10,82 @@ shinyUI(pageWithSidebar(
   # of observations to view
   sidebarPanel(
     
-    numericInput("n", "Number of observations (n):", 1000),
+    numericInput("obs", "Number of observations (n):", 1000),
     # selectInput("dist", "Choose a distribution:", 
     #             choices = c("uniform", "gaussian", "cauchy")),
     selectInput(
       "dist", "Distribution",
       c(Uniform = "uniform",
         Gaussian = "gaussian",
-        Gamma = "gamma")
+        Gamma = "gamma",
+        Cauchy = "cauchy",
+        Bernoulli = "bernoulli",
+        Binomial = "binomial",
+        "Geometric" = "geometric",
+        "Negative Binomial" = "negbinomial",
+        "Poisson" = "poisson")
     ),
     
     conditionalPanel(
       condition = "input.dist == 'uniform'",
-      numericInput("unif_min", "Min (a):", 1),
-      numericInput("unif_max", "Max (b):", 10),
+      numericInput("unif_min", "Min (a):", 1, step = 1),
+      numericInput("unif_max", "Max (b):", 10, step = 1),
       withMathJax(),
       uiOutput('unif_pdf')
     ),
     
     conditionalPanel(
       condition = "input.dist == 'gaussian'",
-      numericInput("gaus_mean", "Mean ($$\\beta$$):", 0),
-      numericInput("gaus_var", "Variance ($$\\sigma^2$$):", 1),
+      numericInput("gaus_mean", "Mean ($$\\beta$$):", 0, step = 0.1),
+      numericInput("gaus_var", "Variance ($$\\sigma^2$$):", 1, step = 0.1),
       withMathJax(),
       uiOutput('gaus_pdf')
     ),
     
     conditionalPanel(
       condition = "input.dist == 'gamma'",
-      numericInput("gamma_shape", "Shape:", 4),
-      numericInput("gamma_scale", "Scale:", 1),
+      numericInput("gamma_shape", "Shape:", 4, step = 0.1),
+      numericInput("gamma_scale", "Scale:", 1, step = 0.1),
       withMathJax(),
       uiOutput('gamma_pdf')
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'cauchy'",
+      numericInput("cauc_loc", "Location:", -2, step = 0.1),
+      numericInput("cauc_scale", "Scale:", 0.5, step = 0.1),
+      withMathJax(),
+      uiOutput('cauchy_pdf')
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'bernoulli'",
+      numericInput("bern_prob", "Probability:", 0.5, step = 0.1, min = 0, max = 1),
+      withMathJax(),
+      uiOutput('bern_pmf')
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'binomial'",
+      numericInput("binom_n", "N:", 10),
+      numericInput("binom_prob", "Probability:", 0.5, step = 0.1, min = 0, max = 1),
+      withMathJax(),
+      uiOutput('binom_pmf')
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'geometric'",
+      numericInput("geom_prob", "Probability:", 0.5, step = 0.1, min = 0, max = 1),
+      withMathJax(),
+      uiOutput('geom_pmf')
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'negbinomial'",
+      numericInput("negbinom_prob", "Probability:", 0.5, step = 0.1, min = 0, max = 1),
+      withMathJax(),
+      uiOutput('negbinom_pmf')
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'poisson'",
+      numericInput("pois_lambda", "Lambda:", 0.5, step = 0.1),
+      withMathJax(),
+      uiOutput('pois_pmf')
     )
   ),
   
@@ -50,6 +94,7 @@ shinyUI(pageWithSidebar(
   mainPanel(
     verbatimTextOutput("summary"),
     plotOutput("distPlot"), 
+    plotOutput("ggPlot"), 
     tableOutput("view")
     
   )
